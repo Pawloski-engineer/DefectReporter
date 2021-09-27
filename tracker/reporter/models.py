f72ca28751c7ab640ba2a2aff776bea18f40d1cc
pkbla <pkblasiak@gmail.com>
2021-09-27 21:31:43 +0200

    location model - parent_location_id can be added

--- tracker/reporter/models.py
+++ tracker/reporter/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
 
 
 class Location(models.Model):
--- tracker/reporter/views.py
+++ tracker/reporter/views.py
@@ -9,49 +9,49 @@ from rest_framework import permissions
 from rest_framework import filters, status
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def locations_list(request):
     if request.method == "GET":
         locations = Location.objects.all()
         serializer = LocationSerializer(locations, many=True)
-        return JsonResponse(serializer.data, safe = False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = LocationSerializer(data=data)
+        serializer = LocationSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def location_detail(request, pk):
     try:
         location = Location.objects.get(pk=pk)
 
     except Location.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "GET":
         serializer = LocationSerializer(location)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = LocationSerializer(location, data=data)
+        serializer = LocationSerializer(location, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         location.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 #
 # @csrf_exempt
